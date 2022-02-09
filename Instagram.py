e0d25fae76ff18fcc272441fd0981c2c0665dabd
Kiran <kiransettyks@gmail.com>
2022-02-09 13:28:35 +0200

    Opens view of all followers Loops through every person to follow Scroll feature added, but requires to be tested

--- Instagram.py
+++ Instagram.py
@@ -27,10 +27,39 @@ class Instagram:
 
     def followers(self, target):
         self.driver.get(target)
+        self.driver.maximize_window()
         element = WebDriverWait(self.driver, 30).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/div')))
         time.sleep(1)
         self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/div').click()
+        to_follow = self.driver.find_element(By.XPATH, '/html/body/div[6]/div/div/div/div[2]/ul').find_elements(By.TAG_NAME, 'li')
+        i = 0
+        follow_count = len(to_follow)
+        print(f'to loop through {to_follow}')
+        while i < follow_count:
+            print(EC.visibility_of_element_located(to_follow[i])) # To be tested if EC can take a selenium object in this manner
+            print(to_follow[i].find_elements(By.CSS_SELECTOR, "button.sqdOP").text) #Text to check if the correct button has been accessed. Will be replaced by clicking on button
+            #Require code to check if the person has already been followed
+            print("A")
+            i += 1
+            print(i)
+            if i == len(to_follow):
+                time.sleep(1)
+                to_follow = self.driver.find_elements(By.CSS_SELECTOR, "ul.jjbaz div li")
+                follow_count = len(to_follow)
+
+    def find_target(self):
+        print("input name of target")
+        name = input()
+        self.driver.get()
+        search_bar = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+        search_bar.send_keys(name)
+        element = WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "div._01UL2 > div.fuqBx > div")))
+        time.sleep(1)
+        search_results = self.driver.find_elements(By.CSS_SELECTOR, "div._01UL2 > div.fuqBx > div")
+        handle = search_results[0].find_element(By.TAG_NAME, 'a').get_attribute('href')
 
 
 
