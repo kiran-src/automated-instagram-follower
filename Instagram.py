921124e54b5c62334ae676cbafe2ec8bc4993b30
Kiran <kiransettyks@gmail.com>
2022-02-10 17:26:43 +0200

    Successfully scrolls to keep every targeted person in view on the screen Loops continuously until all loaded elements of targets to follow are executed Loads in new targets after initial list is executed

--- Instagram.py
+++ Instagram.py
@@ -15,6 +15,7 @@ class Instagram:
 
     def login(self, email, passw):
         self.driver.get("https://www.instagram.com/accounts/login/")
+        print(f"Scroll Height: {self.driver.execute_script('return document.body.scrollHeight')}")
         element = WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, 'input')))
         input_tag = self.driver.find_elements(By.TAG_NAME, 'input')
         input_tag[0].send_keys(email)
@@ -32,34 +33,42 @@ class Instagram:
             EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/div')))
         time.sleep(1)
         self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/div').click()
+        element = WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/div/div/div/div[2]/ul')))
+        time.sleep(1)
         to_follow = self.driver.find_element(By.XPATH, '/html/body/div[6]/div/div/div/div[2]/ul').find_elements(By.TAG_NAME, 'li')
         i = 0
         follow_count = len(to_follow)
-        print(f'to loop through {to_follow}')
+        print(f'to loop through {len(to_follow)}')
+        followers_div = self.driver.find_element(By.XPATH, '/html/body/div[6]/div/div/div')
         while i < follow_count:
-            print(EC.visibility_of_element_located(to_follow[i])) # To be tested if EC can take a selenium object in this manner
-            print(to_follow[i].find_elements(By.CSS_SELECTOR, "button.sqdOP").text) #Text to check if the correct button has been accessed. Will be replaced by clicking on button
-            #Require code to check if the person has already been followed
-            print("A")
-            i += 1
+            if to_follow[i].find_element(By.CSS_SELECTOR, 'div button.sqdOP div').text == 'Follow':
+                # require code to scroll appropriate amount
+                print(to_follow[i].find_element(By.CSS_SELECTOR, "button.sqdOP").is_displayed())
+                to_follow[i].find_element(By.CSS_SELECTOR, "button.sqdOP").click()
+                time.sleep(2)
+                print("Follow status is now " + to_follow[i].find_element(By.CSS_SELECTOR, 'div button.sqdOP div').text)
+                self.driver.execute_script("arguments[0].scrollIntoView()", to_follow[i])
+            else:
+                print("Already Following")
             print(i)
+            i += 1
             if i == len(to_follow):
-                time.sleep(1)
-                to_follow = self.driver.find_elements(By.CSS_SELECTOR, "ul.jjbaz div li")
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)", followers_div)
+                time.sleep(3)
+                to_follow = self.driver.find_element(By.XPATH, '/html/body/div[6]/div/div/div/div[2]/ul').find_elements(By.TAG_NAME, 'li')
                 follow_count = len(to_follow)
+                print(f"End of the line {len(to_follow)}")
 
-    def find_target(self):
-        print("input name of target")
-        name = input()
-        self.driver.get()
-        search_bar = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
-        search_bar.send_keys(name)
-        element = WebDriverWait(self.driver, 30).until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "div._01UL2 > div.fuqBx > div")))
-        time.sleep(1)
-        search_results = self.driver.find_elements(By.CSS_SELECTOR, "div._01UL2 > div.fuqBx > div")
-        handle = search_results[0].find_element(By.TAG_NAME, 'a').get_attribute('href')
-
-
-
+    # def find_target(self):
+    #     print("input name of target")
+    #     name = input()
+    #     self.driver.get()
+    #     search_bar = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+    #     search_bar.send_keys(name)
+    #     element = WebDriverWait(self.driver, 30).until(
+    #         EC.presence_of_element_located(
+    #             (By.CSS_SELECTOR, "div._01UL2 > div.fuqBx > div")))
+    #     time.sleep(1)
+    #     search_results = self.driver.find_elements(By.CSS_SELECTOR, "div._01UL2 > div.fuqBx > div")
+    #     handle = search_results[0].find_element(By.TAG_NAME, 'a').get_attribute('href')
